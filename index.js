f2c3f7ff1011359151b88afc93125ee298788cf3
Michael Soares <mikey.soares@gmail.com>
2018-11-21 16:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments){
-var array = [];
-for (var i = 0; i < musicians.length; i++){
-array.push(`${musicians[i]} plays ${instruments[i]}`);
-}
-return array;
+function theBeatlesPlay(musicians, insturments){
+  var array = []
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${insturments[i]}`)
+  }
+  return array
 }
 
-function johnLennonFacts(facts) {
-  var i = 0;
+function johnLennonFacts(facts){
+  var i = 0
   while (i < facts.length) {
-    facts[i] += "!!!";
-    i++;
+    facts[i] + "!!!"
   }
-  return facts;
+  return facts
 }
+
 function iLoveTheBeatles(n){
   var array = [];
-var i = 0;
-do {
-  array.push("I love the Beatles!");
-  i++;
-} while (i <= n && n < 15);
-return array
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  } while (i <= n && n < 15 ){
+    return array
+  }
 }
